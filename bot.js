178de5c9e4823c6b57b45a5063686c86925a574a
M3KO <30333942+M3KO@users.noreply.github.com>
2017-09-23 15:55:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,10 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^\/cool guy/;
+      botRegexKys = /^\/kys/;
       botRegexRoasted = /^\/roasted/;
       botRegexHelp = /^\/help/;
-      botRegexKys = /^\/kys/;
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
