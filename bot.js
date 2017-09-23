e465c3da361edd81cfd51b2317d375ecbd61f751
M3KO <30333942+M3KO@users.noreply.github.com>
2017-09-23 13:11:48 -0400

    Added /help
    Hopefully /help does not display the 's.

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ function respond() {
       botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSlam = /^\/slam/; botRegexBrye = /^\/brye/;
       botRegexGian = /^\/gian/; botRegexScrotes = /^\/scrotes/; botRegexShaki = /^\/shaki/;
       botRegexDaf = /^\/dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/;
-      botRegexRoasted = /^\/roasted/;
+      botRegexRoasted = /^\/roasted/; botRegexHelp = /^\/help/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -175,6 +175,15 @@ function respond() {
   else if(request.text && botRegexRoasted.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://media.tenor.com/images/6c26eb787cf05133783ac236b2fca0ca/tenor.gif");
+    this.res.end();
+  }
+  else if(request.text && botRegexHelp.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("Here's a list of things you can do:
+                '/help'
+                '/roasted'
+                '/kys'
+                'And more to come in the future!'");
     this.res.end();
   }
   else {
